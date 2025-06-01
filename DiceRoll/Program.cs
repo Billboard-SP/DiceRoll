8ecf2db6d14f8ff2b284b6c7700a54749045b564
Billboard-SP <A00312828@mycambrian.ca>
2025-06-01 18:04:12 -0400

    Added the ability to add more damage types

--- DiceRoll/Program.cs
+++ DiceRoll/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DiceRoll
+﻿using static DiceRoll.DamageCalculator;
+
+namespace DiceRoll
 {
     internal class Program
     {
@@ -19,8 +21,29 @@
                 Console.Write("Enter base damage type: ");
                 string damageType = Console.ReadLine();
 
-                DamageCalculator roll = new DamageCalculator(diceCount, diceSides, modifier, damageType);
+                DamageRoll roll = new DamageRoll(diceCount, diceSides, modifier, damageType);
 
+                // add multiple misc damage sources
+                string addMore;
+                do
+                {
+                    Console.Write("Add extra damage source? (y/n): ");
+                    addMore = Console.ReadLine().ToLower();
+
+                    if (addMore == "y" || addMore == "yes")
+                    {
+                        Console.Write("Enter number of extra dice: ");
+                        int miscCount = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter sides per extra die: ");
+                        int miscSides = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter extra damage type: ");
+                        string miscType = Console.ReadLine();
+
+                        roll.AddMiscDamage(miscCount, miscSides, miscType);
+                    }
+                }
             }
         }
     }
