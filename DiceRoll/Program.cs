b3a874ca889e58b1fe6534638ddde0977bbc590f
Billboard-SP <A00312828@mycambrian.ca>
2025-06-01 17:09:20 -0400

    start of program

--- DiceRoll/Program.cs
+++ DiceRoll/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input;
+            do
+            {
+                Console.Write("Enter number of dice: ");
+                int diceCount = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter number of sides per die: ");
+                int diceSides = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter Modifier: ");
+                int modifier = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter base damage type: ");
+                string damageType = Console.ReadLine();
+
+                DamageCalculator roll = new DamageCalculator(diceCount, diceSides, modifier, damageType);
+
+            }
         }
     }
 }
