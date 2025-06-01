306e273db4efa1c1a5a9c177aac8acf30fcbcbc2
Billboard-SP <A00312828@mycambrian.ca>
2025-06-01 16:29:49 -0400

    made DamageCalculator

---
+++ DiceRoll/DamageCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoll
+{
+    internal class DamageCalculator
+    {
+        public class DamageRoll
+        {
+
+        }
+    }
+}
